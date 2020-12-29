af316e5e55db0861787a3ff22e1fd4fa5d7106cb
mrsdo <me@martidolce.com>
2020-12-29 13:00:16 -0500

    Spec Files Passing

--- app.rb
+++ app.rb
@@ -1,18 +1,29 @@
+# frozen_string_literal: true
+
 require_relative 'config/environment'
 
+# Class App Router
 class App < Sinatra::Base
-
   # This is a sample static route.
   get '/hello' do
-    "Hello World!"
+    'Hello World!'
   end
 
   # This is a sample dynamic route.
-  get "/hello/:name" do
+  get '/hello/:name' do
     @user_name = params[:name]
     "Hello #{@user_name}!"
   end
 
-  # Code your final two routes here:
+  get '/goodbye/:name' do
+    @name = params[:name]
+    "Goodbye, #{@name}."
+  end
 
+  get '/multiply/:num1/:num2' do
+    @num1 = params[:num1]
+    @num2 = params[:num2]
+    @sum = params[:num1].to_i * params[:num2].to_i
+    "The product of #{@num1} x #{@num2} = #{@sum}"
+  end
 end
